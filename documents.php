bc55603c9d02308252ffddad9ca8ca1a72bc10d8
markosamuli <contact@markosamuli.com>
2011-04-10 18:52:05 +0300

    Improved user methods

--- documents.php
+++ documents.php
@@ -85,7 +85,7 @@ class User extends Shanty_Mongo_Document
     
     public function updateTotalTopics()
     {
-        $total = Topic::all($this->getReferenceQuery())->count();
+        $total = $this->getTopics()->count();
         $this->totalTopics = $total;
         $this->save();
         return $total;
@@ -93,7 +93,7 @@ class User extends Shanty_Mongo_Document
     
     public function updateTotalPosts()
     {
-        $total = Post::all($this->getReferenceQuery())->count();
+        $total = $this->getPosts()->count();
         $this->totalPosts = $total;
         $this->save();
         return $total;
@@ -101,10 +101,58 @@ class User extends Shanty_Mongo_Document
     
     public function updateTotalComments()
     {
-        $total = Comment::all($this->getReferenceQuery())->count();
+        $total = $this->getComments()->count();
         $this->totalComments = $total;
         $this->save();
         return $total;
+    }
+    
+    public function getTopics()
+    {
+        return Topic::all($this->getReferenceQuery());
+    }
+    
+    public function getPosts()
+    {
+        return Post::all($this->getReferenceQuery());
+    }
+    
+    public function getComments()
+    {
+        return Comment::all($this->getReferenceQuery());
+    }
+    
+    public function deleteTopics($limit = 100)
+    {
+        $i = 0;
+        $topics = $this->getTopics()->limit($limit);
+        foreach ($topics as $topic) {
+            $topic->delete();
+            $i++;
+        }
+        return $i;
+    }
+    
+    public function deletePosts($limit = 100)
+    {
+        $i = 0;
+        $posts = $this->getPosts()->limit($limit);
+        foreach ($posts as $post) {
+            $post->delete();
+            $i++;
+        }
+        return $i;
+    }
+    
+    public function deleteComments($limit = 100)
+    {
+        $i = 0;
+        $comments = $this->getComments()->limit($limit);
+        foreach ($comments as $comment) {
+            $comment->delete();
+            $i++;
+        }
+        return $i;
     }
     
 }
--- public/reset.php
+++ public/reset.php
@@ -25,43 +25,60 @@ if ($restful) {
 
 if (!empty($user)) {
     
+    /*
     $userRef = MongoDBRef::create(User::getCollectionName(), $user->getId());
     $query = array('user' => $userRef);
+    */
     
     $start = microtime(true);
-    $total = Comment::all($query)->count();
+    $total = $user->getComments()->count();
     echo "Total comments: $total (" . runtime($start) .  "ms)\n";  
     if ($total > 0) {
+        $start = microtime(true);
+        $deleted = $user->deleteComments(1000);
+        echo "Deleted $deleted comments (" . runtime($start) .  "ms)\n"; 
+        /*
         $comments = Comment::all($query)->limit(1000);
         foreach ($comments as $comment) {
             $start = microtime(true);
             $comment->delete();
             echo 'comment:' . $comment->getId() . " (" . runtime($start) .  "ms)\n";               
         }
+        */
     }   
     
     $start = microtime(true);
-    $total = Post::all($query)->count();
+    $total = $user->getPosts()->count();
     echo "Total posts: $total (" . runtime($start) .  "ms)\n";  
     if ($total > 0) {
+        $start = microtime(true);
+        $deleted = $user->deletePosts(1000);
+        echo "Deleted $deleted posts (" . runtime($start) .  "ms)\n";
+        /*
         $posts = Post::all($query)->limit(1000);
         foreach ($posts as $post) {
             $start = microtime(true);
             $post->delete();
             echo 'post:' . $post->getId() . " (" . runtime($start) .  "ms)\n";    
         }
+        */
     }
     
     $start = microtime(true);
-    $total = Topic::all($query)->count();
+    $total = $user->getTopics()->count();
     echo "Total topics: $total (" . runtime($start) .  "ms)\n";  
     if ($total > 0) {
+        $start = microtime(true);
+        $deleted = $user->deleteTopics(1000);
+        echo "Deleted $deleted topics (" . runtime($start) .  "ms)\n";
+        /*
         $topics = Topic::all($query)->limit(1000);
         foreach ($topics as $topic) {
             $start = microtime(true);
             $topic->delete();
             echo 'topic:' . $topic->getId() . " (" . runtime($start) .  "ms)\n";    
         }
+        */
     }
     
     $user->updateTotalTopics();
