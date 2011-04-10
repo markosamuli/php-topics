96d48afd6215c2b6281e8a1be46c4ea08ea52eb6
markosamuli <contact@markosamuli.com>
2011-04-10 22:00:45 +0300

    Improve xml.php

--- public/xml.php
+++ public/xml.php
@@ -1,4 +1,5 @@
 <?php
+
 header('Content-type: text/xml; charset=utf-8');
 require "../init.php";
 
@@ -28,16 +29,39 @@ if ($user === null) {
     header("HTTP/1.1 404 Not Found");
 }
 
+function getBaseUrl($uri = null)
+{
+    $basePath = preg_replace('/xml\.php.*/', '', $_SERVER['REQUEST_URI']);
+    $baseUrl = "http://" . $_SERVER['HTTP_HOST'] . $basePath;
+    return $baseUrl . $uri;
+}
+
 function addPostXml($parentXml, $post)
 {
     $postXml = $parentXml->addChild('post');
-    $postXml->addAttribute('id', $post->getId());       
+    $postXml->addAttribute('id', $post->getId());     
+    $postXml->addAttribute('url', getBaseUrl("xml.php?post=" . $post->getId()));     
     
     $userXml = addUserXml($postXml, $post->user);
     
     $postXml->addChild('message', $post->message);
     $postXml->addChild('created', $post->getCreatedDate()->toString(Zend_Date::RFC_3339));        
     $postXml->addChild('modified', $post->getModifiedDate()->toString(Zend_Date::RFC_3339));
+    
+    if ($imageId = $post->getImageId()) {
+        $imageXml = $postXml->addChild('image');
+        $imageXml->addAttribute('id', $imageId);
+        $imageXml->addChild('created', $post->image->getCreatedDate()->toString(Zend_Date::RFC_3339));
+        $imageXml->addChild('modified', $post->image->getModifiedDate()->toString(Zend_Date::RFC_3339));
+        
+        $urlXml = $imageXml->addChild('assets');
+        // $original = $urlXml->addChild('original');
+        // $original->addAttribute('url', getBaseUrl("image.php?id=" . $imageId));
+        $tiny = $urlXml->addChild('tiny');
+        $tiny->addAttribute('url', getBaseUrl("image.php?id=" . $imageId . "&size=tiny"));
+        $thumb = $urlXml->addChild('thumb');
+        $thumb->addAttribute('url', getBaseUrl("image.php?id=" . $imageId . "&size=thumb"));
+    }
 
     return $postXml;
     
@@ -60,6 +84,7 @@ function addTopicXml($parentXml, $topic)
 {
     $topicXml = $parentXml->addChild('topic');
     $topicXml->addAttribute('id', $topic->getId());  
+    $topicXml->addAttribute('url', getBaseUrl("xml.php?topic=" . $topic->getId())); 
     
     $userXml = addUserXml($topicXml, $topic->user);
     
@@ -79,6 +104,15 @@ function addUserXml($parentXml, $user)
     return $userXml;
 }
 
+function addCommentsXml($parentsXml, $comments)
+{
+    $commentsXml = $parentsXml->addChild('comments');
+    foreach ($comments as $comment) {
+        $commentXml = addCommentXml($commentsXml, $comment);
+    }
+    return $commentsXml;
+}
+
 function addTopicsXml($parentXml, $topics)
 {
     
@@ -92,14 +126,9 @@ function addTopicsXml($parentXml, $topics)
     
         $posts = $topic->getPosts()->limit(10);
         foreach ($posts as $post) {
-        
             $postXml = addPostXml($postsXml, $post);
-        
-            $commentsXml = $postXml->addChild('comments');
+            $commentsXml = addCommentsXml($postXml, $post->getComments());
             $commentsXml->addAttribute('total', (int)$post->totalComments);
-            foreach ($post->getComments() as $comment) {
-                $commentXml = addCommentXml($commentsXml, $comment);
-            }
         }
      
     }
@@ -130,7 +159,11 @@ if (isset($_GET['post'])) {
     
     $post = Post::find($_GET['post']);
     if ($post) {
+        
         $postXml = addPostXml($sxe, $post);
+        $commentsXml = addCommentsXml($postXml, $post->getComments());
+        $commentsXml->addAttribute('total', (int)$post->totalComments);
+        
     } else {
         header("HTTP/1.1 404 Not Found");
     }
